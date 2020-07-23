6ba4690c347d818ef6e0eb66c510117b684a1af3
Krish <krish@sentientgeeks.com>
2020-07-23 18:23:24 +0530

    initial commit

---
+++ src/ios/pdfviewerApp.m
@@ -0,0 +1,43 @@
+/********* pdfviewerApp.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface pdfviewerApp : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+- (void)print:(CDVInvokedUrlCommand*)command;
+@end
+
+@implementation pdfviewerApp
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
+
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (void)print:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
+
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
---
+++ www/pdfviewerApp.js
@@ -0,0 +1,9 @@
+var exec = require('cordova/exec');
+
+exports.coolMethod = function (arg0, success, error) {
+    exec(success, error, 'pdfviewerApp', 'coolMethod', [arg0]);
+};
+
+module.exports.print = function (arg0, success, error) {
+    exec(success, error, 'pdfviewerApp', 'print', [arg0]);
+};
